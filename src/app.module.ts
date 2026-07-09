8ddfa5fb5f74c6e6ecb7068fbc4946809419128a
Apple <apple@Apple-MacBook-Air.local>
2026-07-09 22:59:12 +0300

    Create orders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MenuModule } from './menu/menu.module';
 import { TablesModule } from './tables/tables.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [RestaurantsModule, PrismaModule, UsersModule, AuthModule, MenuModule, TablesModule],
+  imports: [RestaurantsModule, PrismaModule, UsersModule, AuthModule, MenuModule, TablesModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
